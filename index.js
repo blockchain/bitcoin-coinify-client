11c6932a7c43c7233768f2a21b9b939c19f0181a
Sjors Provoost <sjors@sprovoost.nl>
2016-12-07 10:09:04 +0100

    fix(index.js): make coinify.js the entry point

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-module.exports = {
-  Exchange: require('./src/exchange'),
-  API: require('./src/api'),
-  PaymentAccount: require('./src/payment-account'),
-  PaymentMedium: require('./src/payment-medium'),
-  Quote: require('./src/quote'),
-  Trade: require('./src/trade'),
-  Helpers: require('./src/helpers')
-};
+module.exports = require('./src/coinify');
