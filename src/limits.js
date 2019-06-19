e12af3ce731eae10d266bd18b15183d4504fc346
plondon <philip@blockchain.com>
2019-06-19 11:38:17 -0400

    fix(Limit): find method or fallback to null

--- src/limits.js
+++ src/limits.js
@@ -4,16 +4,10 @@ var Limit = require('./limit');
 
 module.exports = Limits;
 
-var findMethod = (method, methods) => methods.find(m => m.inMedium === method);
-
 function Limits (methods) {
-  var card = findMethod('card', methods);
-  var bank = findMethod('bank', methods);
-  var blockchain = findMethod('blockchain', methods);
-  
-  this._card = card ? new Limit(findMethod('card')) : null;
-  this._bank = bank ? new Limit(findMethod('bank')) : null;
-  this._blockchain = blockchain ? new Limit(findMethod('blockchain')) : null;
+  this._card = new Limit(methods.find((method) => method.inMedium === 'card') || {});
+  this._bank = new Limit(methods.find((method) => method.inMedium === 'bank') || {});
+  this._blockchain = new Limit(methods.find((method) => method.inMedium === 'blockchain') || {});
 }
 
 Object.defineProperties(Limits.prototype, {
